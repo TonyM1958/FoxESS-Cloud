5e2f1deeb63c7b73a3c6e8c523f04bc630089635
TonyM1958 <tony.matthews1@outlook.com>
2023-06-06 19:29:07 +0100

    raw data query added

--- foxess.py
+++ foxess.py
@@ -26,8 +26,6 @@ user_agent_rotator = UserAgent(software_names=software_names, operating_systems=
 # global settings and vars
 debug_setting = 1
 
-url_auth = "https://www.foxesscloud.com/c/v0/user/login"
-url_device = "https://www.foxesscloud.com/c/v0/device"
 url_raw = "https://www.foxesscloud.com/c/v0/device/history/raw"
 url_report = "https://www.foxesscloud.com/c/v0/device/history/report"
 
@@ -35,9 +33,11 @@ token = {'value': None, 'valid_from': None, 'valid_for': datetime.timedelta(hour
 device_list = None
 device = None
 device_id = None
+battery = None
 firmware = None
 variables = None
-battery = None
+var_list = None
+
 allData = {
     "report":{},
     "reportDailyGeneration": {},
@@ -45,20 +45,23 @@ allData = {
     "online":False
     }
 
+# login and get token if required. Check if token has expired and renew if required.
 def get_token():
-    global username, password, token
+    global username, password, token, device_list, device, device_id
     time_now = datetime.datetime.now()
     if token['valid_from'] is not None:
         if (time_now - token['valid_from']).seconds <= token['valid_for']:
-            if debug_setting > 0:
+            if debug_setting > 1:
                 print(f"token is still valid")
             return token['value']
     if debug_setting > 0:
         print(f"loading new token")
-    credentials = {'user': private.username, 'password': hashlib.md5(private.password.encode()).hexdigest()}
+    device_list = None
+    device = None
     token['user_agent'] = user_agent_rotator.get_random_user_agent()
     headers = {'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
-    response = requests.post(url=url_auth, headers=headers, data=credentials)
+    credentials = {'user': private.username, 'password': hashlib.md5(private.password.encode()).hexdigest()}
+    response = requests.post(url="https://www.foxesscloud.com/c/v0/user/login", headers=headers, data=json.dumps(credentials))
     if response.status_code != 200:
         print(f"** login response code: {response.status_code}")
         return None
@@ -72,18 +75,19 @@ def get_token():
     token['valid_from'] = time_now
     return token['value']
 
-def get_device():
+# get list of available devices and select one
+def get_device(n=None):
     global token, device_list, device, device_id
-    if device is not None:
-        return device
     if get_token() is None:
         print(f"** could not get a token")
         return None
+    if device is not None:
+        return device
     if debug_setting > 0:
         print(f"getting device")
     headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
-    query = {'pageSize': 10, 'currentPage': 1, 'total': 0, 'queryDate': {'begin': 0, 'end':0} }
-    response = requests.post(url_device + '/list', headers=headers, data=query)
+    query = {'pageSize': 100, 'currentPage': 1, 'total': 0, 'queryDate': {'begin': 0, 'end':0} }
+    response = requests.post(url="https://www.foxesscloud.com/c/v0/device/list", headers=headers, data=json.dumps(query))
     if response.status_code != 200:
         print(f"** list response code: {response.status_code}")
         return None
@@ -91,29 +95,33 @@ def get_device():
     if result is None:
         print(f"** no device list result data")
         return None
-    device_list = result.get('devices')
-    if device_list is None or len(device_list) < 1:
-        print(f"** no devices found")
+    total = result.get('total')
+    if total is None or total == 0 or total > 100:
+        print(f"** invalid list of devices returned: {total}")
         return None
-    if len(device_list) > 1:
-        print(f"** {len(device_list)} devices were found")
+    device_list = result.get('devices')
+    total = len(device_list)
+    if (n is None and total > 1) or (n is not None and n > total):
+        print(f"** {total} devices were found")
         for d in device_list:
             print(f"SN={d['deviceSN']}, Type={d['deviceType']}, ID={d['deviceID']} ")
-    device = device_list[0]
+        return None
+    device = device_list[0 if n is None else n]
     device_id = device.get('deviceID')
+    get_firmware()
+    get_variables()
     return device
 
+# get battery info for selected device
 def get_battery():
-    global token, device_list, device, device_id, battery
+    global token, device_id, battery
     if get_device() is None:
         print(f"** could not get a device")
         return None
-    if battery is not None:
-        return battery
     if debug_setting > 0:
         print(f"getting battery")
     headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
-    response = requests.get(url_device + '/battery/info?id=' + device_id, headers=headers)
+    response = requests.get(url="https://www.foxesscloud.com/c/v0/device/battery/info?id=" + device_id, headers=headers)
     if response.status_code != 200:
         print(f"** battery response code: {response.status_code}")
         return None
@@ -124,17 +132,18 @@ def get_battery():
     battery = result
     return battery
 
+# get list of variables available for selected device
 def get_variables():
-    global token, device_list, device, device_id, variables
+    global token, device_id, variables, var_list
     if get_device() is None:
         print(f"** could not get a device")
         return None
-    if variables is not None:
-        return variables
     if debug_setting > 0:
         print(f"getting variables")
+    var_list = None
     headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
-    response = requests.get(url_device + '/variables?deviceID=' + device_id, headers=headers)
+    # v1 api required for full list with {name, variable, unit}
+    response = requests.get(url="https://www.foxesscloud.com/c/v1/device/variables?deviceID=" + device_id, headers=headers)
     if response.status_code != 200:
         print(f"** variables response code: {response.status_code}")
         return None
@@ -146,8 +155,12 @@ def get_variables():
     if variables is None:
         print(f"** no variables list")
         return None
+    var_list = []
+    for v in variables:
+        var_list.append(v['variable'])
     return variables
 
+# get current firmware versions for selected device
 def get_firmware():
     global token, device_id, firmware
     if get_device() is None:
@@ -156,7 +169,7 @@ def get_firmware():
     if debug_setting > 0:
         print(f"getting firmware")
     headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
-    response = requests.get(url_device + '/addressbook?deviceID=' + device_id, headers=headers)
+    response = requests.get(url="https://www.foxesscloud.com/c/v0/device/addressbook?deviceID=" + device_id, headers=headers)
     if response.status_code != 200:
         print(f"** address book response code: {response.status_code}")
         return None
@@ -170,14 +183,19 @@ def get_firmware():
         return None
     return firmware
 
-
+# get raw data values
 def get_rawdata():
-    if get_addressbook() is None:
-        print(f"** could not get address book")
+    global token, device_id, var_list, debug_setting
+    if get_device() is None:
+        print(f"** could not get device")
         return None
+    if var_list is None:
+        get_variables()
     headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
-    raw = {'deviceID' : private.device_id, 'variables': ['invBatVolt'], 'timespan': 'hour'}
-    response = requests.post(url_raw, headers=headers, data=raw)
+    t = datetime.datetime.now()
+    query = {'deviceID': device_id, 'variables': var_list, 'timespan': 'hour',
+          'beginDate': {'year': t.year, 'month': t.month, 'day': t.day, 'hour': t.hour, 'minute': t.minute, 'second': t.second}}
+    response = requests.post(url="https://www.foxesscloud.com/c/v0/device/history/raw", headers=headers, data=json.dumps(query))
     if response.status_code != 200:
         print(f"** raw data response code: {response.status_code}")
         return None
@@ -186,12 +204,6 @@ def get_rawdata():
         print(f"** no raw data")
         return None
     return result
-
-    rawData = '{"deviceID":"'+deviceID+'","variables":["ambientTemperation","batChargePower","batCurrent","batDischargePower","batTemperature","batVolt","boostTemperation","chargeEnergyToTal","chargeTemperature","dischargeEnergyToTal","dspTemperature","epsCurrentR","epsCurrentS","epsCurrentT","epsPower","epsPowerR","epsPowerS","epsPowerT","epsVoltR","epsVoltS","epsVoltT","feedin","feedin2","feedinPower","generation","generationPower","gridConsumption","gridConsumption2","gridConsumptionPower","input","invBatCurrent","invBatPower","invBatVolt","invTemperation","loads","loadsPower","loadsPowerR","loadsPowerS","loadsPowerT","meterPower","meterPower2","meterPowerR","meterPowerS","meterPowerT","PowerFactor","pv1Current","pv1Power","pv1Volt","pv2Current","pv2Power","pv2Volt","pv3Current","pv3Power","pv3Volt","pv4Current","pv4Power","pv4Volt","pvPower","RCurrent","ReactivePower","RFreq","RPower","RVolt","SCurrent","SFreq","SoC","SPower","SVolt","TCurrent","TFreq","TPower","TVolt"],"timespan":"hour","beginDate":{"year":'+now.strftime(
-        "%Y")+',"month":'+now.strftime("%_m")+',"day":'+now.strftime("%_d")+',"hour":'+now.strftime("%_H")+'}}'
-
-    restRaw = RestData(hass, METHOD_POST, _ENDPOINT_RAW,DEFAULT_ENCODING,
-                       None, headersData, None, rawData, DEFAULT_VERIFY_SSL, SSLCipherList.PYTHON_DEFAULT)
 
 def async_update_data():
     _LOGGER.debug("Updating data from https://www.foxesscloud.com/")
@@ -279,27 +291,4 @@ async def getReportDailyGeneration(hass, headersData, allData, deviceID):
         allData["reportDailyGeneration"] = parsed[0]["data"][int(
             now.strftime("%d")) - 1]
 
-
-async def getRaw(hass, headersData, allData, deviceID):
-    now = datetime.now()
-
-    rawData = '{"deviceID":"'+deviceID+'","variables":["ambientTemperation","batChargePower","batCurrent","batDischargePower","batTemperature","batVolt","boostTemperation","chargeEnergyToTal","chargeTemperature","dischargeEnergyToTal","dspTemperature","epsCurrentR","epsCurrentS","epsCurrentT","epsPower","epsPowerR","epsPowerS","epsPowerT","epsVoltR","epsVoltS","epsVoltT","feedin","feedin2","feedinPower","generation","generationPower","gridConsumption","gridConsumption2","gridConsumptionPower","input","invBatCurrent","invBatPower","invBatVolt","invTemperation","loads","loadsPower","loadsPowerR","loadsPowerS","loadsPowerT","meterPower","meterPower2","meterPowerR","meterPowerS","meterPowerT","PowerFactor","pv1Current","pv1Power","pv1Volt","pv2Current","pv2Power","pv2Volt","pv3Current","pv3Power","pv3Volt","pv4Current","pv4Power","pv4Volt","pvPower","RCurrent","ReactivePower","RFreq","RPower","RVolt","SCurrent","SFreq","SoC","SPower","SVolt","TCurrent","TFreq","TPower","TVolt"],"timespan":"hour","beginDate":{"year":'+now.strftime(
-        "%Y")+',"month":'+now.strftime("%_m")+',"day":'+now.strftime("%_d")+',"hour":'+now.strftime("%_H")+'}}'
-
-    restRaw = RestData(hass, METHOD_POST, _ENDPOINT_RAW,DEFAULT_ENCODING,
-                       None, headersData, None, rawData, DEFAULT_VERIFY_SSL, SSLCipherList.PYTHON_DEFAULT)
-    await restRaw.async_update()
-
-    if restRaw.data is None:
-        _LOGGER.error("Unable to get Raw data from FoxESS Cloud")
-        return False
-    else:
-        _LOGGER.debug("FoxESS Raw data fetched correctly " +
-                      restRaw.data[:150] + " ... ")
-        allData['raw'] = {}
-        for item in json.loads(restRaw.data)['result']:
-            variableName = item['variable']
-            # If data is a non-empty list, pop the last value off the list, otherwise return the previously found value
-            if item["data"]:
-                allData['raw'][variableName] = item["data"].pop().get("value",None)
 
