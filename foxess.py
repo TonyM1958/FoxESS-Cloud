c0ee4e4017c134035221029abf8829040895d0c7
TonyM1958 <tony.matthews1@outlook.com>
2023-06-14 17:33:02 +0100

    Report data, parameterisation

--- foxess.py
+++ foxess.py
@@ -45,6 +45,11 @@ allData = {
     "online":False
     }
 
+
+def query_date(d):
+    t = datetime.datetime.now() if d is None else datetime.datetime.strptime(d, "%Y-%m-%d %H:%M:%S")
+    return {'year': t.year, 'month': t.month, 'day': t.day, 'hour': t.hour, 'minute': t.minute, 'second': t.second}
+
 # login and get token if required. Check if token has expired and renew if required.
 def get_token():
     global username, password, token, device_list, device, device_id
@@ -184,17 +189,19 @@ def get_firmware():
     return firmware
 
 # get raw data values
-def get_rawdata():
+def get_raw(time_span = 'hour', d = None, v = None):
     global token, device_id, var_list, debug_setting
     if get_device() is None:
         print(f"** could not get device")
         return None
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
     if var_list is None:
         get_variables()
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
-    t = datetime.datetime.now()
-    query = {'deviceID': device_id, 'variables': var_list, 'timespan': 'hour',
-          'beginDate': {'year': t.year, 'month': t.month, 'day': t.day, 'hour': t.hour, 'minute': t.minute, 'second': t.second}}
+    if v is None:
+        v = var_list
+    elif type(v) is not list:
+        v = [v]
+    query = {'deviceID': device_id, 'variables': v, 'timespan': time_span, 'beginDate': query_date(d)}
     response = requests.post(url="https://www.foxesscloud.com/c/v0/device/history/raw", headers=headers, data=json.dumps(query))
     if response.status_code != 200:
         print(f"** raw data response code: {response.status_code}")
@@ -205,90 +212,33 @@ def get_rawdata():
         return None
     return result
 
-def async_update_data():
-    _LOGGER.debug("Updating data from https://www.foxesscloud.com/")
-    user_agent = user_agent_rotator.get_random_user_agent()
-    headersData = {"token": token['value'],
-                   "User-Agent": token['user_agent'],
-                   "lang": "en",
-                   "sec-ch-ua-platform": "macOS",
-                   "Sec-Fetch-Site": "same-origin",
-                   "Sec-Fetch-Mode": "cors",
-                   "Sec-Fetch-Dest": "empty",
-                   "Referer": "https://www.foxesscloud.com/bus/device/inverterDetail?id=xyz&flowType=1&status=1&hasPV=true&hasBattery=false",
-                   "Accept-Language":"en-US;q=0.9,en;q=0.8,de;q=0.7,nl;q=0.6",
-                   "X-Requested-With": "XMLHttpRequest"}
-    getAddresbook(hass, headersData, allData, deviceID, username, hashedPassword,0)
-    if int(allData["addressbook"]["result"]["status"]) == 1 or int(allData["addressbook"]["result"]["status"]) == 2 or int(allData["addressbook"]["result"]["status"]) == 3:
-        allData["online"] = True
-        getRaw(hass, headersData, allData, deviceID)
-        getReport(hass, headersData, allData, deviceID)
-        getReportDailyGeneration(hass, headersData, allData, deviceID)
-    return allData
-
-
-
-async def getReport(hass, headersData, allData, deviceID):
-    now = datetime.now()
+# get energy report data in kWh
+def get_report(report_type = 'day', d = None, v = None ):
+    global token, device_id, var_list, debug_setting
+    if get_device() is None:
+        print(f"** could not get device")
+        return None
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    if v is None:
+        v = ['feedin','generation','gridConsumption','chargeEnergyToTal','dischargeEnergyToTal','loads']
+    elif type(v) is not list:
+        v = [v]
+    query = {'deviceID': device_id, 'reportType': report_type, 'variables': v, 'queryDate': query_date(d)}
+    response = requests.post(url="https://www.foxesscloud.com/c/v0/device/history/report", headers=headers, data=json.dumps(query))
+    if response.status_code != 200:
+        print(f"** report data response code: {response.status_code}")
+        return None
+    result = response.json().get('result')
+    if result is None:
+        print(f"** no report data")
+        return None
+    return result
 
 
     reportData = '{"deviceID":"'+deviceID+'","reportType":"day","variables":["feedin","generation","gridConsumption","chargeEnergyToTal","dischargeEnergyToTal","loads"],"queryDate":{"year":'+now.strftime(
         "%Y")+',"month":'+now.strftime("%_m")+',"day":'+now.strftime("%_d")+'}}'
 
-    restReport = RestData(hass, METHOD_POST, _ENDPOINT_REPORT,DEFAULT_ENCODING,
-                          None, headersData, None, reportData, DEFAULT_VERIFY_SSL, SSLCipherList.PYTHON_DEFAULT)
-
-    await restReport.async_update()
-
-    if restReport.data is None:
-        _LOGGER.error("Unable to get Report data from FoxESS Cloud")
-        return False
-    else:
-        _LOGGER.debug("FoxESS Report data fetched correctly " +
-                      restReport.data[:150] + " ... ")
-
-        for item in json.loads(restReport.data)['result']:
-            variableName = item['variable']
-            allData['report'][variableName] = None
-            # Daily reports break down the data hour by hour for the whole day
-            # even if we're only partially through, so sum the values together
-            # to get our daily total so far...
-            cumulative_total = 0
-            for dataItem in item['data']:
-                cumulative_total += dataItem['value']
-            allData['report'][variableName] = cumulative_total
-
-
-async def getReportDailyGeneration(hass, headersData, allData, deviceID):
-    now = datetime.now()
-
     generationData = ('{"deviceID":"' + deviceID + '","reportType": "month",' + '"variables": ["generation"],' + '"queryDate": {' + '"year":' + now.strftime(
         "%Y") + ',"month":' + now.strftime("%_m") + ',"day":' + now.strftime("%_d") + ',"hour":' + now.strftime("%_H") + "}}")
-
-    restGeneration = RestData(
-        hass,
-        METHOD_POST,
-        _ENDPOINT_REPORT,
-        DEFAULT_ENCODING,
-        None,
-        headersData,
-        None,
-        generationData,
-        DEFAULT_VERIFY_SSL,
-        SSLCipherList.PYTHON_DEFAULT
-    )
-
-    await restGeneration.async_update()
-
-    if restGeneration.data is None:
-        _LOGGER.error("Unable to get daily generation from FoxESS Cloud")
-        return False
-    else:
-        _LOGGER.debug("FoxESS daily generation data fetched correctly " +
-                      restGeneration.data)
-
-        parsed = json.loads(restGeneration.data)["result"]
-        allData["reportDailyGeneration"] = parsed[0]["data"][int(
-            now.strftime("%d")) - 1]
 
 
