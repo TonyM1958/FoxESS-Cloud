aca053aca6a44b06fde71241ea6cb6604ee4f7c8
TonyM1958 <tony.matthews1@outlook.com>
2023-07-10 12:00:50 +0100

    more debug info

--- foxess.py
+++ foxess.py
@@ -26,7 +26,7 @@ user_agent_rotator = UserAgent(software_names=software_names, operating_systems=
 # global settings and vars
 debug_setting = 1
 
-token = {'value': None, 'valid_from': None, 'valid_for': datetime.timedelta(hours=4).seconds, 'user_agent': None}
+token = {'value': None, 'valid_from': None, 'valid_for': datetime.timedelta(hours=4).seconds, 'user_agent': None, 'lang': 'en'}
 
 def query_date(d):
     t = datetime.datetime.now() if d is None else datetime.datetime.strptime(d, "%Y-%m-%d %H:%M:%S")
@@ -46,7 +46,7 @@ def get_token():
     device_list = None
     device = None
     token['user_agent'] = user_agent_rotator.get_random_user_agent()
-    headers = {'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    headers = {'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     credentials = {'user': private.username, 'password': hashlib.md5(private.password.encode()).hexdigest()}
     response = requests.post(url="https://www.foxesscloud.com/c/v0/user/login", headers=headers, data=json.dumps(credentials))
     if response.status_code != 200:
@@ -78,7 +78,7 @@ def get_device(n=None):
         return device
     if debug_setting > 0:
         print(f"getting device")
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     query = {'pageSize': 100, 'currentPage': 1, 'total': 0, 'queryDate': {'begin': 0, 'end':0} }
     response = requests.post(url="https://www.foxesscloud.com/c/v0/device/list", headers=headers, data=json.dumps(query))
     if response.status_code != 200:
@@ -117,7 +117,7 @@ def get_vars():
         return None
     if debug_setting > 0:
         print(f"getting variables")
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     params = {'deviceID': device_id}
     # v1 api required for full list with {name, variable, unit}
     response = requests.get(url="https://www.foxesscloud.com/c/v1/device/variables", params=params, headers=headers)
@@ -145,7 +145,7 @@ def get_firmware():
         return None
     if debug_setting > 0:
         print(f"getting firmware")
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     params = {'deviceID': device_id}
     response = requests.get(url="https://www.foxesscloud.com/c/v0/device/addressbook", params=params, headers=headers)
     if response.status_code != 200:
@@ -172,7 +172,7 @@ def get_battery():
         return None
     if debug_setting > 0:
         print(f"getting battery")
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     params = {'id': device_id}
     response = requests.get(url="https://www.foxesscloud.com/c/v0/device/battery/info", params=params, headers=headers)
     if response.status_code != 200:
@@ -194,7 +194,7 @@ def get_charge():
         return None
     if debug_setting > 0:
         print(f"getting charge times")
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     params = {'sn': device_sn}
     response = requests.get(url="https://www.foxesscloud.com/c/v0/device/battery/time/get", params=params, headers=headers)
     if response.status_code != 200:
@@ -222,7 +222,7 @@ def set_charge():
         return None
     if debug_setting > 0:
         print(f"setting charge times")
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     data = {'sn': device_sn, 'times': battery_settings.get('times')}
     response = requests.post(url="https://www.foxesscloud.com/c/v0/device/battery/time/set", headers=headers, data=json.dumps(data))
     if response.status_code != 200:
@@ -242,8 +242,8 @@ def get_min():
         print(f"** could not get a device")
         return None
     if debug_setting > 0:
-        print(f"getting min soc settings")
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+        print(f"getting min soc")
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     params = {'sn': device_sn}
     response = requests.get(url="https://www.foxesscloud.com/c/v0/device/battery/soc/get", params=params, headers=headers)
     if response.status_code != 200:
@@ -268,7 +268,7 @@ def set_min():
         return None
     if debug_setting > 0:
         print(f"setting min soc")
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     data = {'minGridSoc': battery_settings['minGridSoc'], 'minSoc': battery_settings['minSoc'], 'sn': device_sn}
     response = requests.post(url="https://www.foxesscloud.com/c/v0/device/battery/soc/set", headers=headers, data=json.dumps(data))
     if response.status_code != 200:
@@ -298,7 +298,7 @@ def get_work_mode():
         return None
     if debug_setting > 0:
         print(f"getting work mode")
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     params = {'id': device_id, 'hasVersionHead': 1, 'key': 'operation_mode__work_mode'}
     response = requests.get(url="https://www.foxesscloud.com/c/v0/device/setting/get", params=params, headers=headers)
     if response.status_code != 200:
@@ -331,7 +331,7 @@ def set_work_mode(mode):
         return None
     if debug_setting > 0:
         print(f"setting work mode")
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     data = {'id': device_id, 'key': 'operation_mode__work_mode', 'values': {'operation_mode__work_mode': mode}, 'raw': ''}
     response = requests.post(url="https://www.foxesscloud.com/c/v0/device/setting/set", headers=headers, data=json.dumps(data))
     if response.status_code != 200:
@@ -355,13 +355,15 @@ def get_raw(time_span = 'hour', d = None, v = None):
     if get_device() is None:
         print(f"** could not get device")
         return None
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     if v is None:
         if raw_vars is None:
             raw_vars = get_vars()
         v = [x['variable'] for x in raw_vars]
     elif type(v) is not list:
         v = [v]
+    if debug_setting > 0:
+        print(f"getting raw data")
     query = {'deviceID': device_id, 'variables': v, 'timespan': time_span, 'beginDate': query_date(d)}
     response = requests.post(url="https://www.foxesscloud.com/c/v0/device/history/raw", headers=headers, data=json.dumps(query))
     if response.status_code != 200:
@@ -396,11 +398,13 @@ def get_report(report_type = 'day', d = None, v = None ):
     if get_device() is None:
         print(f"** could not get device")
         return None
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     if v is None:
         v = report_vars
     elif type(v) is not list:
         v = [v]
+    if debug_setting > 0:
+        print(f"getting report data")
     query = {'deviceID': device_id, 'reportType': report_type, 'variables': v, 'queryDate': query_date(d)}
     response = requests.post(url="https://www.foxesscloud.com/c/v0/device/history/report", headers=headers, data=json.dumps(query))
     if response.status_code != 200:
@@ -423,7 +427,9 @@ def get_earnings():
     if get_device() is None:
         print(f"** could not get device")
         return None
-    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': 'en', 'Connection': 'keep-alive'}
+    if debug_setting > 0:
+        print(f"getting earnings")
+    headers = {'token': token['value'], 'User-Agent': token['user_agent'], 'lang': token['lang'], 'Connection': 'keep-alive'}
     params = {'deviceID': device_id}
     response = requests.get(url="https://www.foxesscloud.com/c/v0/device/earnings", params=params, headers=headers)
     if response.status_code != 200:
