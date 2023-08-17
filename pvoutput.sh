7cbbcb72abd6bb0e2492363a4b8e9c0811aacd5f
TonyM1958 <tony.matthews1@outlook.com>
2023-08-17 09:19:23 +0100

    Add today param to pvoutput.sh

--- pvoutput.sh
+++ pvoutput.sh
@@ -1,10 +1,9 @@
 #! /usr/bin/zsh
-: ${1:=.}
 pip install random-user-agent --quiet --root-user-action=ignore
-cd $1
+cd ${1:-.}
 python << END
 
 import foxess as f
-f.set_pvoutput(tou=1)
+f.set_pvoutput(tou=1, today=${2:-False})
 
 END
